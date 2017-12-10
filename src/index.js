9564c7bde70404749f564b35494cf38d69dfa6d4
Chris Jarvis <jrvscm@gmail.com>
2017-12-10 12:21:22 -0700

    final

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Provider from 'react-redux';
-
+import { Provider } from 'react-redux';
 import './reset.css';
 import './index.css';
-
 import Game from './components/game';
+import store from './store';
 
 ReactDOM.render(
-<Provider>
+<Provider store={store}>
   <Game />
 </Provider>,
 document.getElementById('root')
